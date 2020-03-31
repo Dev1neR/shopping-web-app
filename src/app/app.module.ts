bd05b4443d4acbe98f9e774721edc4381b6f9875
Dev1neR <theserega18@gmail.com>
2020-03-31 17:44:32 +0300

    Firebase connected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Firebase
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -11,6 +18,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+	AngularFireModule.initializeApp(environment.firebaseConfig, 'shopping-web-app'),
+	AngularFirestoreModule, // Only required for database features
+	AngularFireAuthModule, // Only required for auth features,
+	AngularFireStorageModule // Only required for storage features
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,16 @@
 
 export const environment = {
   production: false
+  const firebaseConfig = {
+	apiKey: "AIzaSyC5rMiPlP_2KER--T15vnDMfh0FsDWrQ1k",
+	authDomain: "shopping-backend-firebas-ee369.firebaseapp.com",
+	databaseURL: "https://shopping-backend-firebas-ee369.firebaseio.com",
+	projectId: "shopping-backend-firebas-ee369",
+	storageBucket: "shopping-backend-firebas-ee369.appspot.com",
+	messagingSenderId: "323211508842",
+	appId: "1:323211508842:web:40ec9a9be563251422de13",
+	measurementId: "G-TFCY58X4PB"
+};
 };
 
 /*
